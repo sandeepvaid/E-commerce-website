5ee0187d8de0f40516a0cf9e8eec992ef77e3438
sandeepvaid <50510974+sandeepvaid@users.noreply.github.com>
2020-11-27 12:50:25 +0530

    Update settings.py

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -121,9 +121,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
